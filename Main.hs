ecdf2a4387ace786abb2a4e265df07a5d3c3f157
Cee <35766903+CBroemse@users.noreply.github.com>
2019-09-09 04:09:25 +0100

    Update Main.hs

--- Main.hs
+++ Main.hs
@@ -11,8 +11,12 @@ import qualified Patternfile as P
 import qualified UsefulFunctions  as Us   
 import qualified WriteWXmaximaFile as WX
 import qualified Diff_Functions1 as Df
+import qualified GUIMenuGHCtext as GUI
+import qualified Data.Time as T
+
 ---------------------------------------
 --Global Variables
+howMany = "100"
 valL = "0.56"
 mymax = (maximum (dropWhile (\(val) -> val < criterium ) ( dipfade2)))
 criterium = "0.67"
@@ -22,19 +26,97 @@ xX = "3" -- howMany lines to process
 -- Level 0                     | GUI-Mode vs RAW-Mode  |
 -- Level 1                     | print outpt yes or no | 
 -- Level 2        |  GUI yes | GUI no  | RAW yes  | RAW no | 
-main :: IO ()
+xRaw = "seniot.txt"
+nF = "seniot2.txt"
+crit = valL
+rnd = "100"
+fohans= "1"
+dit = do
+   foRandTime <- T.getCurrentTime
+   let parameters =  lines ((P.timeRNDInts foRandTime (length (lines "34\n 45\n 67")) 4))
+   P.avanti parameters
+
+main :: IO () 
 main = do
   putStrLn "Enter csv file to read"
   theCsv <- getLine
   putStrLn "Enter file to write e.g:\"seniot.txt\""
   theWriteTxt <- getLine
-   
-  P.writePatternFile theCsv "senio1.txt"
+  input0 <- getLine        
+  let chart0 = if input0=="1"
+               then do 
+                  input <- getLine 
+                  let chart1 = if input=="1"
+                               then do 
+                                  P.writePatternFile theCsv "senio1.txt"
+                                  (chart0)
+                               else if input=="2"
+                               then do
+                                  P.aMemoryRAW "1" xRaw theWriteTxt
+                                  (chart0)
+                               else if input=="3"
+                               then do
+                        -- set yes-output, read theWritetxt -> writes 
+                                  P.aOsZilloskop1RAW "1" xRaw nF crit rnd howMany fohans
+                                  (chart0)
+                               else if input=="4"
+                               then do
+                                  P.aCrunchList1  xRaw nF howMany crit
+                                  (chart0)
+                               else if input=="5"
+                               then do
+                                  putStrLn "End Compute"
+                               else do
+                                  (chart0)
+                  chart1
+               else if input0=="2" ---------------------------------------------------------
+               then do              -- "2" prints NO-OUTPUT
+                  input <- getLine 
+                  let chart2 = if input=="1"
+                               then do 
+                                  P.writePatternFile theCsv "senio1.txt"
+                                  (chart0)
+                               else if input=="2"
+                               then do
+                                  P.aMemoryRAW "2" xRaw theWriteTxt
+                                  (chart0)
+                               else if input=="3"
+                               then do
+                        -- set yes-output
+                                  P.aOsZilloskop1RAW "2" xRaw nF crit rnd howMany fohans
+                                  (chart0)
+                               else if input=="4"
+                               then do
+                                  P.aCrunchList1  xRaw nF howMany crit
+                                  (chart0)
+                               else if input=="5"
+                               then do
+                                  putStrLn "End Compute" -- only very basic Data printed
+                               else do
+                                  (chart0)  
+                  chart2            
+               else 
+                 (main) 
+           -- uses input0 to stop computations  
+  chart0 
+ -- P.writePatternFile theCsv "senio1.txt"
   -- will just succed for shorter list aprx 300 
-  P.aMemory "senio1.txt" (theWriteTxt)
+ -- P.aMemoryRAW "1" "senio1.txt" (theWriteTxt) 
 
  -- aCsvToWx
-  print "Done"
+ -- print "Done"
+
+guiReturnfunction back1step backAllsteps u = 
+                     let ans x1 = if x1=="1" 
+                                  then do 
+                                    back1step
+                                  else
+                                    backAllsteps
+                     in do  -- with do ghi sees line1 ...guiReturn, line2 ..input1... 
+                        (P.foOutput u GUI.guiReturnFunctiontext)
+                        inputgui1 <- getLine
+                        ans inputgui1
+
 -----------------------------------------------------
 
 -----------------------------------------------------
